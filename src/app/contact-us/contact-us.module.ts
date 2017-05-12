f9673eb448a4186c480555d3703d2c47a7df35d8
Max Lychko <maxlychko@gmail.com>
2017-05-12 18:09:10 +0300

    contac-us not completed

--- src/app/contact-us/contact-us.module.ts
+++ src/app/contact-us/contact-us.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactUsRoutes } from './contact-us.routing';
 
@@ -8,15 +9,18 @@ import { ContactUsService } from './contact-us.service';
 
 import { ContactUsComponent } from './contact-us.component';
 import { ContactUsFormComponent } from './contact-us-form/contact-us-form.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(ContactUsRoutes)
   ],
   declarations: [
     ContactUsComponent,
-    ContactUsFormComponent
+    ContactUsFormComponent,
+    ContactsComponent
   ],
   providers: [ ContactUsService ]
 })
