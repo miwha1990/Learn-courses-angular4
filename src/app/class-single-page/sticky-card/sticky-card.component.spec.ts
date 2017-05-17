8f993554620c7d1e33509ac707f7c0a364c54699
anna <tkachovaanna1987@gmail.com>
2017-05-17 17:18:29 +0300

    sticky card component

---
+++ src/app/class-single-page/sticky-card/sticky-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StickyCardComponent } from './sticky-card.component';
+
+describe('StickyCardComponent', () => {
+  let component: StickyCardComponent;
+  let fixture: ComponentFixture<StickyCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StickyCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StickyCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/class-single-page/sticky-card/sticky-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky-card',
+  templateUrl: './sticky-card.component.html',
+  styleUrls: ['./sticky-card.component.scss']
+})
+export class StickyCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
