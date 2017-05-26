df37625be4dd88294211b29f6f1af8f7f33a1af7
Anna Tkachova <vobatukina@yandex.ru>
2017-05-26 13:58:51 +0300

    better sticky version

--- src/app/class-single-page/class-single-page.component.ts
+++ src/app/class-single-page/class-single-page.component.ts
@@ -29,12 +29,10 @@ export class ClassSinglePageComponent implements OnInit {
         .on('sticky_kit:bottom', function(e) {
           $(this).parent().css('position', 'static');
           $('#sticky-element').removeClass("is_stuck");
-          $('#sticky-element').parent().css('position', 'static');;
         })
         .on('sticky_kit:unbottom', function(e) {
           $(this).parent().css('position', 'relative');
             $('#sticky-element').addClass("is_stuck");
-            $('#sticky-element').parent().css('position', 'relative');
         });
   }
 }
