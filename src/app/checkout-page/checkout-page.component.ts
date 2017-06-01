21d3904ece8d24cf30d100ed94da173308a25b14
anna <tkachovaanna1987@gmail.com>
2017-06-01 14:27:09 +0300

    emails match

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrderProcessService} from '../services/order-process.service';
 @Component({
   selector: 'app-checkout-page',
@@ -7,11 +7,55 @@ import { OrderProcessService} from '../services/order-process.service';
   styleUrls: ['./checkout-page.component.scss'],
 })
 export class CheckoutPageComponent implements OnInit {
-  public checkoutForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', Validators.compose([Validators.required, Validators.email])]
-  });
-  constructor(private OrderProcessService: OrderProcessService, private fb: FormBuilder) { }
+
+  checkoutForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      // matching_Emails: this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      confirm_email: ['', [Validators.required, Validators.email]],
+      // }, this.matchingEmails('matching_Emails')),
+      // class_id: [''],
+      // name_of_badge: [''],
+      // gender: [''],
+      // date_of_birth: [''],
+}, {validator: this.matchingPasswords('email', 'confirm_email')});
+  // emailsGroup = this.checkoutForm.controls.matching_Emails;
+
+  constructor(private OrderProcessService: OrderProcessService, private fb: FormBuilder) {
+    //
+    // this.checkoutForm = this.fb.group({
+    //     first_name: ['', Validators.required],
+    //     last_name: ['', Validators.required],
+    //     matching_Emails: this.fb.group({
+    //       email: ['', Validators.compose([Validators.required, Validators.email])],
+    //       confirm_email: ['', Validators.compose([Validators.required, Validators.email])],
+    //     }, {this.matchingEmails('matching_Emails')}),
+    //     class_id: [''],
+    //     name_of_badge: [''],
+    //     gender: [''],
+    //     date_of_birth: [''],
+    //   });
+    //   this.emailsGroup = this.checkoutForm.controls.matching_Emails;
+    //
+
+
+
+  }
+
+
+  matchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    console.log(1111111111)
+    return (group: FormGroup) => {
+      const passwordInput = group.controls[passwordKey];
+      const passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true});
+      }
+    };
+  }
+
+
   ngOnInit() {
   }
   formSubmit() {
@@ -23,4 +67,49 @@ export class CheckoutPageComponent implements OnInit {
       this.formSubmit();
     }
   }
+  // matchingEmails(keyName: string): any {
+  //   console.log(this.checkoutForm);
+  //   const group = this.checkoutForm.controls[keyName];
+  //   const email = group.controls.email;
+  //   const confirm_email = group.controls.confirm_email;
+  //
+  //   // Don't kick in until user touches both fields
+  //   if (email.pristine || confirm_email.pristine) {
+  //     return null;
+  //   }
+  //
+  //   // Mark group as touched so we can add invalid class easily
+  //   group.markAsTouched();
+  //
+  //   if (email.value === confirm_email.value) {
+  //     return null;
+  //   }
+  //
+  //   return {
+  //     isValid: false
+  //   };
+  // }
 }
+// “class_id”: 1,
+  //     "name_on_badge": "Name",
+  //     "gender": "male",
+  //     "date_of_birth": "2000-01-24", # always YYYY-MM-DD
+// "address":
+// {
+//   "city": "Toronto",
+//     "province": "Ontario",
+//     "country": "Canada",
+// },
+// "phone": "+1 (234) 567-8900",
+//     "industry_certification": "xxx",
+//     "employer": "company name",
+//     "job_title": "title",
+//     "emergency_name": "Full Name",
+//     "emergency_phone": "+1 (234) 567-8900",
+//     "refferel": "text",
+//     "tshirt_size": "40-42",
+//     "full_payment": True, # False if 'Pay Deposit Only' was selected
+// "amount": "302.50", # from 'Total' on 'Checkout' moqup
+// "stripe_source": "string_token", # from stripe.js
+// "coupon_code": "code," # can be null,
+// “waiver_id”: “string_id” # id of the shawn waiver
