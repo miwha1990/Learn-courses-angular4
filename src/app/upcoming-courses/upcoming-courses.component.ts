28fbead7343061eaa4f57570b7f702b0438dd1aa
anna <tkachovaanna1987@gmail.com>
2017-06-27 09:42:59 +0300

    spots format

--- src/app/upcoming-courses/upcoming-courses.component.ts
+++ src/app/upcoming-courses/upcoming-courses.component.ts
@@ -1,12 +1,35 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-upcoming-courses',
   templateUrl: './upcoming-courses.component.html',
   styleUrls: ['./upcoming-courses.component.scss']
 })
-export class UpcomingCoursesComponent {
+export class UpcomingCoursesComponent implements  OnChanges {
   @Input() upcomingCoursesData;
   @Input() additionalInfo: boolean = false;
   @Input() textMessage: string;
+  cardContentEarly: string;
+  cardContentRegular: string;
+  ngOnChanges() {
+    if (this.upcomingCoursesData != null) {
+// this.data.partner_url = 'https://stretchtowin.site-ym.com/events/register.aspx?id=909951&itemid=cf22259c-ca1d-41f4-9f2e-a769417b1680';
+      if ( this.upcomingCoursesData.partner_url != null ) {
+        if ( this.upcomingCoursesData.partner_soldout ) {
+          this.cardContentRegular = this.cardContentEarly = 'Sold Out';
+        }
+      } else {
+        if ( this.upcomingCoursesData.spots_early > 0 ) {
+          this.cardContentEarly = this.upcomingCoursesData.spots_early + ' spots left';
+        } else {
+          this.cardContentEarly = 'Sold Out';
+        }
+        if ( this.upcomingCoursesData.spots_regular > 0 ) {
+          this.cardContentRegular = this.upcomingCoursesData.spots_regular + ' spots left';
+        } else {
+          this.cardContentRegular = 'Sold Out';
+        }
+      }
+    }
+  }
 }
